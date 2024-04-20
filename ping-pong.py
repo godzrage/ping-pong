6de73e2fb825d7cd3ce6bbd4e0f47a0df6ae8946
godzrage <masonchik221@gmail.com>
2024-04-20 17:52:29 +0400

    new score and bug fix

--- ping-pong.py
+++ ping-pong.py
@@ -40,61 +40,77 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+win_score = 3
+
+p1 = 0
+p2 = 0
+
 racket_width = 50
 racket_height = 150
 
 #создания мяча и ракетки   
 racket1 = Player('redracket.png', 30, win_height / 2 - racket_height / 2, 4, racket_width, racket_height)
 racket2 = Player('greenracket.png', win_width - racket_width - 30, win_height / 2 - racket_height / 2, 4, racket_width, racket_height)
-ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+ball = GameSprite('tenis_ball.png', win_width/ 2 - 25, win_height /2 - 25, 4, 50, 50)
 
 font.init()
 font = font.Font(None, 35)
 lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
 lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
+
 speed_x = 3
 speed_y = 3
 
 while game:
-   for e in event.get():
-       if e.type == QUIT:
-           game = False
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
   
-   if finish != True:
-       window.fill(back)
-       racket1.update_l()
-       racket2.update_r()
-       ball.rect.x += speed_x
-       ball.rect.y += speed_y
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
-       if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
-           speed_x *= -1
-           speed_y *= 1
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
       
        #если мяч достигает границ экрана, меняем направление его движения
-       if ball.rect.y > win_height-50 or ball.rect.y < 0:
-           speed_y *= -1
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
 
 
        #если мяч улетел дальше ракетки, выводим условие проигрыша для первого игрока
-       if ball.rect.x < 0:
-           finish = True
-           window.blit(lose1, (200, 200))
-           game_over = True
+        if ball.rect.x < 0:
+            p2 += 1
+            ball.rect.x = win_width / 2 - 50/2
+            ball.rect.y = win_height / 2 - 50/2
+            speed_x *= -1
 
 
        #если мяч улетел дальше ракетки, выводим условие проигрыша для второго игрока
-       if ball.rect.x > win_width:
-           finish = True
-           window.blit(lose2, (200, 200))
-           game_over = True
+        if ball.rect.x > win_width:
+            p1 += 1
+            ball.rect.x = win_width / 2 - 50/2
+            ball.rect.y = win_height / 2 - 50/2
+            speed_x *= -1
 
+        score = font.render(str(p1) +":"+ str(p2),True,(180,0,0))
+        window.blit(score,(450,20))
 
-       racket1.reset()
-       racket2.reset()
-       ball.reset()
+        if p1 >= win_score or p2 >= win_score:
+            if p1 >= win_score:
+                window.blit(lose2,(450,300))
+            if p2 >= win_score:
+                window.blit(lose1,(450,300))
+            finish = True
+            game = True
+        
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
 
-
-   display.update()
-   clock.tick(FPS)
+    display.update()
+    clock.tick(FPS)
